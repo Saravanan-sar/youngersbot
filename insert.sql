b640df953c9b933d75d91eae03d40d4a849426b3
Saravanan-sar <saravanannagappan2004@gmail.com>
2025-08-11 17:09:10 +0530

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE employee_dD;
+CREATE TABLES employess(
+   employee_id  int primary key;  
+   name  VARCHAR (100);
+   email VARCHAR (255);
+   image  null;
+   salary  INT;
+)
+   
+  
